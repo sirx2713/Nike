deacfe1733acfc7feadb29240b50dc342d966956
iSAD <bradx2713@gmail.com>
2024-09-15 23:16:13 +0200

    Learning

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -11,6 +11,29 @@ class Landingpage extends StatefulWidget {
 }
 
 class _LandingpageState extends State<Landingpage> {
+  Color iconColor = Colors.grey;
+  IconData selectedIcon = Icons.check;
+  Color sizeColor = Color(0xffF5F5F5);
+  Color textColor = Colors.black;
+  Color sizeColor2 = Color(0xffF5F5F5);
+  Color textColor2 = Colors.black;
+  Color sizeColor3 = Color(0xffF5F5F5);
+  Color textColor3 = Colors.black;
+  Color sizeColor4 = Color(0xffF5F5F5);
+  Color textColor4 = Colors.black;
+  Color sizeColor5 = Color(0xffF5F5F5);
+  Color textColor5 = Colors.black;
+
+  double ukSize = 6;
+  double ukSize2 = 6.5;
+  double ukSize3 = 7.5;
+  double ukSize4 = 8;
+  double ukSize5 = 8.5;
+  Color textLabel = Colors.grey;
+  Color textLabel2 = Colors.grey;
+  Color textLabel3 = Colors.grey;
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,7 +52,7 @@ class _LandingpageState extends State<Landingpage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     GestureDetector(
-                      onTap: (){
+                      onTap: () {
                         runApp(const Onboarding());
                       },
                       child: const Icon(Icons.arrow_back_ios_new_outlined),
@@ -42,16 +65,24 @@ class _LandingpageState extends State<Landingpage> {
                       width: 60,
                     ),
                     const SizedBox(
-                      width: 120,
+                      width: 110,
                     ),
-                    const Icon(
-                      Icons.favorite,
-                      color: Colors.grey,
+                    GestureDetector(
+                      onTap: () {
+                        iconColor = Colors.red;
+                        setState(() {});
+                      },
+                      child: Icon(
+                        Icons.favorite,
+                        color: iconColor,
+                      ),
                     )
                   ],
                 ),
               ),
-              const SizedBox(height: 100,),
+              const SizedBox(
+                height: 100,
+              ),
               const Image(image: AssetImage("images/shoe.png")),
               Container(
                 alignment: Alignment.topLeft,
@@ -104,8 +135,8 @@ class _LandingpageState extends State<Landingpage> {
                     decoration: BoxDecoration(
                         color: const Color(0xff557C56),
                         borderRadius: BorderRadius.circular(30)),
-                    child: const Icon(
-                      Icons.check,
+                    child: Icon(
+                      selectedIcon,
                       color: Colors.white,
                     ),
                   ),
@@ -152,32 +183,77 @@ class _LandingpageState extends State<Landingpage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "Uk".toUpperCase(),
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
+                      GestureDetector(
+                        onTap: (){
+                          ukSize = 6;
+                          ukSize2 = 6.5;
+                          ukSize3 = 7.5;
+                          ukSize4 = 8;
+                          ukSize5 = 8.5;
+                          textLabel = Colors.black;
+                          textLabel2 = Colors.grey;
+                          textLabel3 = Colors.grey;
+                          setState(() {
+
+                          });
+                        },
+                        child: Text(
+                          "Uk".toUpperCase(),
+                          style: TextStyle(
+                              color: textLabel,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                       const SizedBox(
                         width: 10,
                       ),
-                      Text(
-                        "Us".toUpperCase(),
-                        style: const TextStyle(
-                            color: Colors.grey,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
+                      GestureDetector(
+                        onTap: (){
+                          ukSize = 8;
+                          ukSize2 = 8.5;
+                          ukSize3 = 9;
+                          ukSize4 = 9.5;
+                          ukSize5 = 10;
+                          textLabel2 = Colors.black;
+                          textLabel = Colors.grey;
+                          textLabel3 = Colors.grey;
+                          setState(() {
+
+                          });
+                        },
+                        child: Text(
+                          "Us".toUpperCase(),
+                          style: TextStyle(
+                              color: textLabel2,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                       const SizedBox(
                         width: 10,
                       ),
-                      Text(
-                        "eU".toUpperCase(),
-                        style: const TextStyle(
-                            color: Colors.grey,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
+                      GestureDetector(
+                        onTap: (){
+                          ukSize = 38;
+                          ukSize2 = 39;
+                          ukSize3 = 40;
+                          ukSize4 = 41;
+                          ukSize5 = 42;
+                          textLabel3 = Colors.black;
+                          textLabel2 = Colors.grey;
+                          textLabel = Colors.grey;
+                          setState(() {
+
+                          });
+                        },
+                        child: Text(
+                          "eU".toUpperCase(),
+                          style: TextStyle(
+                              color: textLabel3,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ],
                   ),
@@ -189,77 +265,176 @@ class _LandingpageState extends State<Landingpage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Container(
-                    alignment: Alignment.center,
-                    height: 60,
-                    width: 70,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: const Color(0xffF5F5F5)),
-                    child: const Text(
-                      "8.5",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                  GestureDetector(
+                    onTap: (){
+                      sizeColor = Colors.black;
+                      textColor = Colors.white;
+
+                      sizeColor2 = Color(0xffF5F5F5);
+                      textColor2 = Colors.black;
+
+                      sizeColor3 = Color(0xffF5F5F5);
+                      textColor3 = Colors.black;
+
+                      sizeColor4 = Color(0xffF5F5F5);
+                      textColor4 = Colors.black;
+
+                      sizeColor5 = Color(0xffF5F5F5);
+                      textColor5 = Colors.black;
+                      setState(() {
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 60,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color:  sizeColor),
+                      child: Text(
+                        ukSize.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold, color: textColor),
+                      ),
                     ),
                   ),
                   const SizedBox(
-                    width: 10,
+                    width: 5,
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    height: 60,
-                    width: 70,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: const Color(0xffF5F5F5)),
-                    child: const Text(
-                      "9",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                  GestureDetector(
+                    onTap: (){
+                      sizeColor2 = Colors.black;
+                      textColor2 = Colors.white;
+
+                      sizeColor = Color(0xffF5F5F5);
+                      textColor = Colors.black;
+
+                      sizeColor3 = Color(0xffF5F5F5);
+                      textColor3 = Colors.black;
+
+                      sizeColor4 = Color(0xffF5F5F5);
+                      textColor4 = Colors.black;
+
+                      sizeColor5 = Color(0xffF5F5F5);
+                      textColor5 = Colors.black;
+                      setState(() {
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 60,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: sizeColor2),
+                      child: Text(
+                        ukSize2.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold, color: textColor2),
+                      ),
                     ),
                   ),
                   const SizedBox(
-                    width: 10,
+                    width: 5,
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    height: 60,
-                    width: 70,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.black),
-                    child: const Text(
-                      "9.5",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.white),
+                  GestureDetector(
+                    onTap: (){
+                      sizeColor3 = Colors.black;
+                      textColor3 = Colors.white;
+
+                      sizeColor2 = Color(0xffF5F5F5);
+                      textColor2 = Colors.black;
+
+                      sizeColor = Color(0xffF5F5F5);
+                      textColor = Colors.black;
+
+                      sizeColor4 = Color(0xffF5F5F5);
+                      textColor4 = Colors.black;
+
+                      sizeColor5 = Color(0xffF5F5F5);
+                      textColor5 = Colors.black;
+                      setState(() {
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 60,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: sizeColor3),
+                      child: Text(
+                        ukSize3.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold, color: textColor3),
+                      ),
                     ),
                   ),
                   const SizedBox(
-                    width: 10,
+                    width: 5,
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    height: 60,
-                    width: 70,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: const Color(0xffF5F5F5)),
-                    child: const Text(
-                      "10",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                  GestureDetector(
+                    onTap: (){
+                      sizeColor4 = Colors.black;
+                      textColor4 = Colors.white;
+
+                      sizeColor2 = Color(0xffF5F5F5);
+                      textColor2 = Colors.black;
+
+                      sizeColor3 = Color(0xffF5F5F5);
+                      textColor3 = Colors.black;
+
+                      sizeColor = Color(0xffF5F5F5);
+                      textColor = Colors.black;
+
+                      sizeColor5 = Color(0xffF5F5F5);
+                      textColor5 = Colors.black;
+                      setState(() {
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 60,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: sizeColor4),
+                      child: Text(
+                          ukSize4.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold, color: textColor4)
+                      ),
                     ),
                   ),
                   const SizedBox(
-                    width: 10,
+                    width: 5,
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    height: 60,
-                    width: 70,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: const Color(0xffF5F5F5)),
-                    child: const Text(
-                      "10.5",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                  GestureDetector(
+                    onTap: (){
+                      sizeColor5 = Colors.black;
+                      textColor5 = Colors.white;
+
+                      sizeColor2 = Color(0xffF5F5F5);
+                      textColor2 = Colors.black;
+
+                      sizeColor3 = Color(0xffF5F5F5);
+                      textColor3 = Colors.black;
+
+                      sizeColor4 = Color(0xffF5F5F5);
+                      textColor4 = Colors.black;
+
+                      sizeColor = Color(0xffF5F5F5);
+                      textColor = Colors.black;
+                      setState(() {
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 60,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: sizeColor5),
+                      child: Text(
+                        ukSize5.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold, color: textColor5),
+                      ),
                     ),
                   ),
                 ],
@@ -274,16 +449,20 @@ class _LandingpageState extends State<Landingpage> {
                     "R799.99",
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                   ),
-                  const SizedBox(width: 100,),
+                  const SizedBox(
+                    width: 100,
+                  ),
                   Container(
                     alignment: Alignment.center,
                     height: 60,
                     width: 150,
                     decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(60)
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(60)),
+                    child: const Text(
+                      "Add to Bag",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    child: const Text("Add to Bag", style: TextStyle(color: Colors.white),),
                   )
                 ],
               )
